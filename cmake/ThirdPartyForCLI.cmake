55542f690d9189f215c7e41d426885d0ceedcca5
TianZerL <TianLin2112@outlook.com>
2020-05-23 18:04:10 +0800

    Try to fix build error of G++-8

--- cmake/ThirdPartyForCLI.cmake
+++ cmake/ThirdPartyForCLI.cmake
@@ -30,3 +30,9 @@ find_package(OpenCL REQUIRED)
 include_directories(${TOP_DIR}/ThirdParty/include ${OpenCL_INCLUDE_DIRS})
 
 target_link_libraries(${PROJECT_NAME} Anime4KCPPCore)
+
+if(NOT WIN32)
+target_link_libraries(${PROJECT_NAME} stdc++fs)
+endif()
+
+
