7d795f2466cd5cfc2ed7c65afa8ae28fb738d70b
TianZerL <TianLin2112@outlook.com>
2020-09-11 13:26:54 +0800

    Update cmake Support for specifying OpenCL SDK provider

--- cmake/ThirdPartyForASP.cmake
+++ cmake/ThirdPartyForASP.cmake
@@ -1,6 +1,10 @@
 include_directories(${AviSynthPlus_SDK_PATH}/include)
 
-target_link_directories(${PROJECT_NAME} PRIVATE ${AviSynthPlus_SDK_PATH}/lib/x64)
+if(OS_64_Bit)
+    target_link_directories(${PROJECT_NAME} PRIVATE ${AviSynthPlus_SDK_PATH}/lib/x64)
+else()
+    target_link_directories(${PROJECT_NAME} PRIVATE ${AviSynthPlus_SDK_PATH}/lib/x86)
+endif()
 
 target_link_libraries(${PROJECT_NAME} AviSynth)
 
--- cmake/ThirdPartyForVS.cmake
+++ cmake/ThirdPartyForVS.cmake
@@ -1,6 +1,10 @@
 include_directories(${VapourSynth_SDK_PATH}/include)
 
-target_link_directories(${PROJECT_NAME} PRIVATE ${VapourSynth_SDK_PATH}/lib64)
+if(OS_64_Bit)
+    target_link_directories(${PROJECT_NAME} PRIVATE ${VapourSynth_SDK_PATH}/lib64)
+else()
+    target_link_directories(${PROJECT_NAME} PRIVATE ${VapourSynth_SDK_PATH}/lib32)
+endif()
 
 target_link_libraries(${PROJECT_NAME} vapoursynth)
 
