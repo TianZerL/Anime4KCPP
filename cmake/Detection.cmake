0f36eb56027a22502694d3050a0935abb0abdc08
TianZerL <TianLin2112@outlook.com>
2020-10-01 12:00:44 +0800

    Update cmake

--- cmake/Detection.cmake
+++ cmake/Detection.cmake
@@ -1,18 +1,20 @@
 set(TMP_DIR ${PROJECT_BINARY_DIR}/tmp)
 set(DetectionSample_DIR ${TOP_DIR}/cmake/DetectionSample)
 
-if(NOT EXISTS ${DetectionSample_DIR}/has_filesystem.cpp)
-    file(MAKE_DIRECTORY ${DetectionSample_DIR})
-    file(TOUCH ${DetectionSample_DIR}/has_filesystem.cpp)
-    file(WRITE ${DetectionSample_DIR}/has_filesystem.cpp 
+set(has_filesystem_cpp
 "#include <filesystem>
 
 int main()
 {
-    std::filesystem::path hasFS(\"./\");
-    return 0;
+    std::filesystem::path hasFS(\"./\")\;
+    return 0\;
 }
 ")
+
+if(NOT EXISTS ${DetectionSample_DIR}/has_filesystem.cpp)
+    file(MAKE_DIRECTORY ${DetectionSample_DIR})
+    file(TOUCH ${DetectionSample_DIR}/has_filesystem.cpp)
+    file(WRITE ${DetectionSample_DIR}/has_filesystem.cpp ${has_filesystem_cpp})
 
     # check std::filesystem
     if(CMAKE_CXX_COMPILER_ID MATCHES "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9.0)
