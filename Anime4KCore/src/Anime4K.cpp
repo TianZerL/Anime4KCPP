a3eb884f1b8fd38c072ee1eb0a508d929d79f48b
TianZerL <TianLin2112@outlook.com>
2020-10-26 14:42:44 +0800

    Update core Update showImg

--- Anime4KCore/src/Anime4K.cpp
+++ Anime4KCore/src/Anime4K.cpp
@@ -446,13 +446,13 @@ void Anime4KCPP::Anime4K::showImage(bool R2B)
 
     if (inputYUV)
     {
-        if (dstY.size() == dstU.size() && dstU.size() == dstV.size())
-        {
-            cv::merge(std::vector<cv::Mat>{ dstY, dstU, dstV }, tmpImg);
-            cv::cvtColor(tmpImg, tmpImg, cv::COLOR_YUV2BGR);
-        }
-        else
-            throw ACException<ExceptionType::IO>("Only YUV444 can be saved to file");
+        cv::Mat tmpU, tmpV;
+        if (dstY.size() != dstU.size())
+            cv::resize(dstU, tmpU, dstY.size(), 0.0, 0.0, cv::INTER_LANCZOS4);
+        if (dstY.size() != dstV.size())
+            cv::resize(dstV, tmpV, dstY.size(), 0.0, 0.0, cv::INTER_LANCZOS4);
+        cv::merge(std::vector<cv::Mat>{ dstY, tmpU, tmpV }, tmpImg);
+        cv::cvtColor(tmpImg, tmpImg, cv::COLOR_YUV2BGR);
     }
 
     if (checkAlphaChannel)
