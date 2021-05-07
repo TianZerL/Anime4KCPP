256e3401fa8ab030172e9cd1b1af501030ff75a4
TianZerL <TianLin2112@outlook.com>
2021-05-08 03:28:57 +0800

    Update core Compatible with OpenCL 1.1

--- Anime4KCore/src/OpenCLAnime4K09.cpp
+++ Anime4KCore/src/OpenCLAnime4K09.cpp
@@ -7,8 +7,8 @@
 #define CL_HPP_TARGET_OPENCL_VERSION 120
 #else
 #define CL_HPP_TARGET_OPENCL_VERSION 200
-#define CL_HPP_MINIMUM_OPENCL_VERSION 110
 #endif // LEGACY_OPENCL_API
+#define CL_HPP_MINIMUM_OPENCL_VERSION 110
 #include<opencl.hpp>
 
 #include"FilterProcessor.hpp"
