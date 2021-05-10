a0ed236c1130d88e1c93bd428ef784d1d1f38482
TianZerL <TianLin2112@outlook.com>
2021-05-11 01:44:08 +0800

    Update CMake

--- cmake/ThirdPartyForCWrapper.cmake
+++ cmake/ThirdPartyForCWrapper.cmake
@@ -2,7 +2,7 @@ find_package(OpenCL REQUIRED)
 
 include_directories(${TOP_DIR}/ThirdParty/include ${OpenCL_INCLUDE_DIRS})
 
-if(NOT Build_C_wrapper_with_core)
+if(NOT Build_C_Wrapper_With_Core)
     target_link_libraries(${PROJECT_NAME} Anime4KCPPCore)
 else()
     include(${TOP_DIR}/cmake/ThirdPartyForCore.cmake)
