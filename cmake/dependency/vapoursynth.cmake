c2efecf7669d221b55e812cf55cae3b21dd7754d
TianZerL <TianLin2112@outlook.com>
2024-04-24 17:53:28 +0800

    Update CMake bug fixes

--- cmake/dependency/vapoursynth.cmake
+++ cmake/dependency/vapoursynth.cmake
@@ -1,10 +1,17 @@
 if(NOT TARGET dep::vapoursynth)
     add_library(dep_vapoursynth INTERFACE IMPORTED)
-    if(WIN32)    
+    if(WIN32)
+        if(AC_COMPILER_32BIT)
+            set(AC_PATH_VAPOURSYNTH_LIB_SUBDIR lib32)
+            set(AC_URL_VAPOURSYNTH_RELEASE https://github.com/vapoursynth/vapoursynth/releases/download/R63/VapourSynth32-Portable-R63.7z)
+        else()
+            set(AC_PATH_VAPOURSYNTH_LIB_SUBDIR lib64)
+            set(AC_URL_VAPOURSYNTH_RELEASE https://github.com/vapoursynth/vapoursynth/releases/download/R66/VapourSynth64-Portable-R66.zip)
+        endif()
         if(NOT AC_PATH_VAPOURSYNTH_SDK)
             FetchContent_Declare(
                 vapoursynth
-                URL https://github.com/vapoursynth/vapoursynth/releases/download/R66/VapourSynth64-Portable-R66.zip
+                URL ${AC_URL_VAPOURSYNTH_RELEASE}
                 CONFIGURE_COMMAND ""
                 BUILD_COMMAND ""
                 TEST_COMMAND ""
@@ -14,7 +21,7 @@ if(NOT TARGET dep::vapoursynth)
         endif()
         find_library(dep_vapoursynth_LIBS 
             NAMES vapoursynth
-            HINTS ${AC_PATH_VAPOURSYNTH_SDK}
+            HINTS ${AC_PATH_VAPOURSYNTH_SDK}/${AC_PATH_VAPOURSYNTH_LIB_SUBDIR}
             REQUIRED
         )
         target_link_libraries(dep_vapoursynth INTERFACE ${dep_vapoursynth_LIBS})
