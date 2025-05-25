d11cc010cd0f800168a5fe86c8c88ac4f7151108
TianZerL <TianLin2112@outlook.com>
2025-05-25 19:27:51 +0800

    Update CMake - Enhance OpenCL HPP header check

--- cmake/CheckOpenCLHPP.cmake
+++ cmake/CheckOpenCLHPP.cmake
@@ -1,6 +1,12 @@
 include(CheckCXXCompilerFlag)
 
 set(CMAKE_REQUIRED_LIBRARIES OpenCL::OpenCL)
+if (dep_opencl_TARGET_VERSION)
+    set(CHECK_OPENCL_HPP_TARGET_VERSION 300)
+else()
+    set(CHECK_OPENCL_HPP_TARGET_VERSION ${dep_opencl_TARGET_VERSION})
+endif()
+set(CMAKE_REQUIRED_DEFINITIONS -DCL_HPP_TARGET_OPENCL_VERSION=${CHECK_OPENCL_HPP_TARGET_VERSION} -DCL_HPP_MINIMUM_OPENCL_VERSION=110)
 check_cxx_source_compiles("
 #if __has_include(<CL/opencl.hpp>)
 #   include <CL/opencl.hpp>
@@ -11,4 +17,6 @@ check_cxx_source_compiles("
 #endif
 int main() { cl::size_type a = 0; return 0; }
 " dep_opencl_SUPPORT_HPP)
+unset(CMAKE_REQUIRED_DEFINITIONS)
+unset(CHECK_OPENCL_HPP_TARGET_VERSION)
 unset(CMAKE_REQUIRED_LIBRARIES)
