a7ec93963609321aef6b4fea782df1babf5a56f9
TianZerL <TianLin2112@outlook.com>
2025-05-27 00:13:53 +0800

    Update core - Align stride to 4 bytes by default

--- core/src/Image.cpp
+++ core/src/Image.cpp
@@ -5,8 +5,11 @@
 #include "AC/Core/Image.hpp"
 #include "AC/Core/Util.hpp"
 
-#ifndef AC_MALLOC_ALIGN
-#   define AC_MALLOC_ALIGN 32
+#ifndef AC_CORE_MALLOC_ALIGN
+#   define AC_CORE_MALLOC_ALIGN 32
+#endif
+#ifndef AC_CORE_STRIDE_ALIGN
+#   define AC_CORE_STRIDE_ALIGN 4
 #endif
 
 namespace ac::core::detail
@@ -19,9 +22,9 @@ namespace ac::core::detail
 
     inline static void* alignedAlloc(const std::size_t size) noexcept
     {
-        auto buffer = static_cast<void**>(std::malloc(size + sizeof(void*) + AC_MALLOC_ALIGN));
+        auto buffer = static_cast<void**>(std::malloc(size + sizeof(void*) + AC_CORE_MALLOC_ALIGN));
         if (!buffer) return nullptr;
-        void** ptr = alignPtr(buffer + 1, AC_MALLOC_ALIGN);
+        void** ptr = alignPtr(buffer + 1, AC_CORE_MALLOC_ALIGN);
         ptr[-1] = static_cast<void*>(buffer);
         return ptr;
     }
@@ -60,7 +63,7 @@ void ac::core::Image::create(const int w, const int h, const int c, const Elemen
 {
     int lineSize = w * c * (elementType & 0xff);
     if (!(h > 0 && lineSize > 0)) return;
-    int pitch = stride > lineSize ? stride : lineSize;
+    int pitch = stride >= lineSize ? stride : align(lineSize, AC_CORE_STRIDE_ALIGN);
     int size = h * pitch;
     this->w = w;
     this->h = h;
@@ -74,7 +77,7 @@ void ac::core::Image::map(const int w, const int h, const int c, const ElementTy
 {
     int lineSize = w * c * (elementType & 0xff);
     if (!(h > 0 && lineSize > 0) || !data) return;
-    int pitch = stride > lineSize ? stride : lineSize;
+    int pitch = stride >= lineSize ? stride : lineSize;
     this->w = w;
     this->h = h;
     this->c = c;
@@ -87,7 +90,7 @@ void ac::core::Image::from(const int w, const int h, const int c, const ElementT
 {
     int lineSize = w * c * (elementType & 0xff);
     if (!(h > 0 && lineSize > 0) || !data) return;
-    int pitch = stride > lineSize ? stride : lineSize;
+    int pitch = stride >= lineSize ? stride : lineSize;
     create(w, h, c, elementType);
     auto src = static_cast<const std::uint8_t*>(data);
     for (int i = 0; i < h; i++) std::memcpy(line(i), src + i * pitch, lineSize);
@@ -96,7 +99,7 @@ void ac::core::Image::to(void* const data, const int stride) const noexcept
 {
     int lineSize = width() * pixelSize();
     if (!(height() > 0 && lineSize > 0) || !data) return;
-    int pitch = stride > lineSize ? stride : lineSize;
+    int pitch = stride >= lineSize ? stride : lineSize;
     auto dst = static_cast<std::uint8_t*>(data);
     for (int i = 0; i < height(); i++) std::memcpy(dst + i * pitch, line(i), lineSize);
 }
