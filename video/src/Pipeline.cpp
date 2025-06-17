806b76f2ea3fd3da9460106eedf45353fa579fed
TianZerL <TianLin2112@outlook.com>
2025-06-17 17:33:26 +0800

    Update video module

--- video/src/Pipeline.cpp
+++ video/src/Pipeline.cpp
@@ -125,6 +125,7 @@ namespace ac::video::detail
         default: break;
         }
         encoderCtx->bit_rate = hints.bitrate > 0 ? hints.bitrate : static_cast<decltype(encoderCtx->bit_rate)>(decoderCtx->bit_rate * factor * factor);
+        encoderCtx->rc_max_rate = encoderCtx->bit_rate / 4 * 5;
         encoderCtx->framerate = decoderCtx->framerate;
         encoderCtx->gop_size = static_cast<decltype(encoderCtx->gop_size)>(10 * av_q2d(decoderCtx->framerate) + 0.5); // 10s gop size, maybe we should use dynamic gop size.
         encoderCtx->time_base = timeBase;
